3249901661daf323a1d174f2cb8330129b82cf6b
AnnaKoufou <58466433+AnnaKoufou@users.noreply.github.com>
2020-04-01 03:30:15 +0200

    Add files via upload

---
+++ IdeaProjects/learning java/src/lerning.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class lerning {
+    public static void main(String[]args){
+        int x=1;
+
+        do {
+            System.out.println(x++);
+        }while (x>5);
+
+    }
+}
+
